3fb33cee095884dc9b6a081ef39ac7e6fdcd7332
Maxime Grenier <maxdeoxis@gmail.com>
2024-10-23 21:25:16 +0200

    feat(wish): add a route for testing open ai vision

--- src/wishlist/routes.ts
+++ src/wishlist/routes.ts
@@ -1,25 +1,56 @@
 import { Router } from "@oak/oak/router";
 import { supabase } from "../../supabase.ts";
+import OpenAI from "openai";
+const openai = new OpenAI({ apiKey: Deno.env.get("OPENAI_SECRET") });
 
 const router = new Router({ prefix: "/list" });
 
+// console.log(Deno.env.get("OPENAI_SECRET"))
+
+// router.get("/bababa", async ({ response }) => {
+//     const completion = await openai.chat.completions.create({
+//         model: "gpt-4o-mini",
+//         messages: [
+//             { role: "user", content: [
+//                 {
+//                     type: "text", text: "Here is a screenshot of an ecommerce site. Give me a JSON data with two properties: the name of the item, and its price. If you can't find the item nor the price, return an empty json."
+//                 },
+//                 {
+//                     type: "image_url",
+//                     image_url: {
+//                         // "url": "https://image.noelshack.com/fichiers/2024/43/3/1729709559-screenamazontest.png",
+//                         "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+//                     },
+//                 }
+//             ] },
+//         ],
+//     });
+
+//     console.log(completion)
+//     response.body = completion
+// })
+
 router.get("/", async (ctx) => {
-    const { data } = await supabase.from("list").select("*, wish (*)");
+    const { data } = await supabase.from("list").select("*, wishes (*)");
     ctx.response.body = data;
 });
 
 router.get("/:hash", async (ctx) => {
-    const { data } = await supabase.from("list").select("*, wish(*)").eq("hash", ctx.params.hash).maybeSingle();
+    const { data } = await supabase.from("list").select("*, wishes(*)").order('created_at', {
+        referencedTable: 'wishes',
+        ascending: false
+    }
+    ).eq("hash", ctx.params.hash).maybeSingle();
     if (!data) {
         ctx.response.status = 404
     }
     ctx.response.body = data;
 });
 
-router.get("/:hash/wish/:wishId", async ({ params, response }) => {
+router.get("/:hash/wishes/:wishId", async ({ params, response }) => {
     const { hash, wishId } = params;
 
-    const data = await supabase.from("wish").select("*").eq("id", wishId).maybeSingle()
+    const data = await supabase.from("wishes").select("*").eq("id", wishId).maybeSingle()
     if (!data) {
         response.status = 404
     }
@@ -43,10 +74,10 @@ router.post("/", async ({ request, response }) => {
     response.body = data;
 });
 
-router.post("/wish", async ({ request, response }) => {
+router.post("/wishes", async ({ request, response }) => {
     const { url, name, listId } = await request.body.json();
 
-    const { data, error } = await supabase.from("wish").insert({
+    const { data, error } = await supabase.from("wishes").insert({
         url,
         name,
         listId,
@@ -59,11 +90,11 @@ router.post("/wish", async ({ request, response }) => {
     response.body = data;
 })
 
-router.put("/:hash/wish/:wishId/toggle", async ({ params, response, request }) => {
+router.put("/:hash/wishes/:wishId/toggle", async ({ params, response, request }) => {
     const { hash, wishId } = params;
     const { bought_by } = await request.body.json();
 
-    const wish = await supabase.from("wish").select("*").eq("id", wishId).maybeSingle()
+    const wish = await supabase.from("wishes").select("*").eq("id", wishId).maybeSingle()
 
     if (!wish) {
         response.status = 404
@@ -74,13 +105,12 @@ router.put("/:hash/wish/:wishId/toggle", async ({ params, response, request }) =
     }
 
     try {
-        const data = await supabase.from("wish").update({ bought_by }).eq("id", wishId).select()
+        const data = await supabase.from("wishes").update({ bought_by }).eq("id", wishId).select()
         response.body = data;
 
     } catch (e) {
         console.error(e)
     }
-
 })
 
 router.allowedMethods();
